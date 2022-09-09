69d37dd328d5aa1bd1e55fc1ed7e79ca322653de
Victor <Victor>
2022-09-09 01:50:31 -0700

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,51 +1,16 @@
 #include <stdio.h>
 
-#include <conio.h>
-
-main()
-
+/**
+ *main - main block
+ *Return: 0
+ */
+ 
+int main(void)
 {
-
-		clrscr();
-
-			printf("    short int is %2d bytes \n", sizeof(short int));
-
-				printf("          int is %2d bytes \n", sizeof(int));
-
-					printf("        int * is %2d bytes \n", sizeof(int *));
-
-						printf("     long int is %2d bytes \n", sizeof(long int));
-
-							printf("   long int * is %2d bytes \n", sizeof(long int *));
-
-								printf("   signed int is %2d bytes \n", sizeof(signed int));
-
-									printf(" unsigned int is %2d bytes \n", sizeof(unsigned int));
-
-										printf("\n");
-
-											printf("        float is %2d bytes \n", sizeof(float));
-
-												printf("      float * is %2d bytes \n", sizeof(float *));
-
-													printf("       double is %2d bytes \n", sizeof(double));
-
-														printf("     double * is %2d bytes \n", sizeof(double *));
-
-															printf("  long double is %2d bytes \n", sizeof(long double));
-
-																printf("\n");
-
-																	printf("  signed char is %2d bytes \n", sizeof(signed char));
-
-																		printf("         char is %2d bytes \n", sizeof(char));
-
-																			printf("       char * is %2d bytes \n", sizeof(char *));
-
-																				printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
-
-																					getch();
-
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
-
-
