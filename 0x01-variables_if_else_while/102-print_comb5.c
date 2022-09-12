9afe09ec00b4d8693da99dcae6be7bc18e983f31
Victor <Victor>
2022-09-12 16:41:43 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,8 @@ for (j = i + 1; j <= 99; j++)
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
-putchar(j / 10 + '0');								putchar(j % 10 + '0')
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 if (!(i == 98 && j == 99))
 {
 putchar(',');
