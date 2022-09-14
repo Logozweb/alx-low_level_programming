823eeac1cd021365fc6c1139e38148dc46ad51d3
Victor <Victor>
2022-09-14 03:16:25 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ *add - adds to ints
+ *
+ *@a: one integer
+ *
+ *@b: another integer
+ *
+ *
+ *
+ *Return: an int = a+b
+ *
+ */
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
