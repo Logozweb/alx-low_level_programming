b106c6206115ddccc382922177dc4db69dfe5db9
Victor <Victor>
2022-09-19 14:42:06 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,30 +1,18 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-
-
-
-/**
- *main - check the code 
- *
- *
- *
- *Return: Always 0.
- *
- */
-
-int main(void)
-{
-int n;
-
-n = 402;
-
-printf("n=%d\n", n);
-
-reset_to_98(&n);
-
-printf("n=%d\n", n);
-
-return (0);
-
-}
+#include<stdio.h>
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif /* MAIN_H */
